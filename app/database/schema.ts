db52d8016611a2263773478c641cee229469c020
Patricia <patyfb04@gmail.com>
2025-12-29 16:48:54 -0500

    adding trasaction schema

--- app/database/schema.ts
+++ app/database/schema.ts
@@ -1,5 +1,8 @@
+import { time } from "console";
 import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
+import { relations } from "drizzle-orm";
+import z from "zod";
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
@@ -8,6 +11,10 @@ export const accounts = pgTable("accounts", {
   plaidId: text("plaid_id").default(""),
   created_at: timestamp("created_at").defaultNow().notNull(),
 });
+
+export const accountsRelations = relations(accounts, ({ many }) => ({
+  transations: many(transactions),
+}));
 
 export const insertAccountSchema = createInsertSchema(accounts);
 
@@ -18,12 +25,44 @@ export const categories = pgTable("categories", {
   plaidId: text("plaid_id").default(""),
 });
 
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  transations: many(transactions),
+}));
+
 export const insertCategorySchema = createInsertSchema(categories);
 
-// export const transactions = pgTable("transactions", {
-//   id: serial("id").primaryKey(),
-//   amount: text("amount").notNull(),
-// });
+export const transactions = pgTable("transactions", {
+  id: text("id").primaryKey(),
+  amount: text("amount").notNull(),
+  payee: text("payee").notNull(),
+  notes: text("notes"),
+  date: timestamp("date", { mode: "date" }).notNull(),
+  accountId: text("account_id")
+    .references(() => accounts.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  categoryId: text("category_id")
+    .references(() => categories.id, {
+      onDelete: "set null",
+    })
+    .notNull(),
+});
+
+export const transactionsRelations = relations(transactions, ({ one }) => ({
+  account: one(accounts, {
+    fields: [transactions.accountId],
+    references: [accounts.id],
+  }),
+  category: one(categories, {
+    fields: [transactions.categoryId],
+    references: [categories.id],
+  }),
+}));
+
+export const insertTransationSchema = createInsertSchema(transactions, {
+  date: z.coerce.date(),
+});
 
 // export const users = pgTable("users", {
 //   id: text("id").primaryKey(),
