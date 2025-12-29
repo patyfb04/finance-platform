a8eb7f79637f739d264b4823f0a91ae73976c71d
Patricia <patyfb04@gmail.com>
2025-12-29 14:23:34 -0500

    fix patch route

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -19,5 +19,8 @@ const routes = app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+export const PATCH = handle(app);
+export const DELETE = handle(app);
+export const PUT = handle(app);
 
 export type AppType = typeof routes;
