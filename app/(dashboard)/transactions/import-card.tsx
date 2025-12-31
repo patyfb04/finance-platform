08f77af083730d8dc92623f639ab51f54edbff6f
Patricia <patyfb04@gmail.com>
2025-12-31 16:56:09 -0500

    finalize import catr- transactions

--- app/(dashboard)/transactions/import-card.tsx
+++ app/(dashboard)/transactions/import-card.tsx
@@ -2,6 +2,7 @@ import { Card, CardContent, CardTitle, CardHeader } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { ImportTable } from "./import-table";
+import { format, parse } from "date-fns";
 
 const dateFormat = "yyyy-MM-dd HH:mm:ss";
 const outputFormat = "yyyy-MM-dd";
@@ -44,6 +45,45 @@ export const ImportCard = ({ data, onCancel, onSubmit }: Props) => {
   };
   const progress = Object.values(selectedColumns).filter(Boolean).length;
 
+  const handleContinue = () => {
+    const getColumnIndex = (column: string) => {
+      return column.split("_")[1];
+    };
+    const mappedData = {
+      headers: headers.map((_header, index) => {
+        const columnIndex = getColumnIndex(`column_${index}`);
+        return selectedColumns[`column_${columnIndex}`] || null;
+      }),
+      body: body
+        .map((row, index) => {
+          const transformedRow = row.map((cell, index) => {
+            const columnIndex = getColumnIndex(`column_${index}`);
+            return selectedColumns[`column_${columnIndex}`] ? cell : null;
+          });
+          return transformedRow.every((item) => item === null)
+            ? []
+            : transformedRow;
+        })
+        .filter((row) => row.length > 0),
+    };
+    const arrayOfData = mappedData.body.map((row) => {
+      return row.reduce((acc: any, cell, index) => {
+        const header = mappedData.headers[index];
+        if (header !== null) {
+          acc[header] = cell;
+        }
+        return acc;
+      }, {});
+    });
+
+    const formattedData = arrayOfData.map((item) => ({
+      ...item,
+      amount: parseFloat(item.amount.trim()),
+      date: format(parse(item.date, dateFormat, new Date()), outputFormat),
+      test: item.amount.trim(),
+    }));
+  };
+
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <Card className="border-none drop-shadow-sm">
@@ -56,7 +96,7 @@ export const ImportCard = ({ data, onCancel, onSubmit }: Props) => {
               Cancel
             </Button>
             <Button
-              onClick={() => {}}
+              onClick={handleContinue}
               size="sm"
               disabled={progress < requiredOptions.length}
               className="w-full lg:w-auto"
