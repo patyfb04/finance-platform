11bb0b3d2b0b818bd87f821d76b3437b189f4638
Patricia <patyfb04@gmail.com>
2026-01-07 17:52:51 -0500

    fix dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,16 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
   test: {
-    environment: "jsdom", // or 'happy-dom'
-    setupFiles: ["./vitest.setup.ts"],
+    environment: "jsdom",
     globals: true,
+    setupFiles: "./vitest.setup.ts",
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "."),
+      "@": path.resolve(__dirname, "./"),
     },
   },
 });
